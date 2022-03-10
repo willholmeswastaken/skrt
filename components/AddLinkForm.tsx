0da935eaef4dfca3da5839036db4e046388e7d97
Will Holmes <willholmes97@icloud.com>
2022-03-10 17:47:03 +0000

    Added validation to form

--- components/AddLinkForm.tsx
+++ components/AddLinkForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 interface IFormInput {
@@ -15,8 +15,14 @@ interface IAddLinkFormProps {
 }
 
 const AddLinkForm: React.FC<IAddLinkFormProps> = ({ setLink }) => {
-  const { register, handleSubmit, reset } = useForm<IFormInput>();
-
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitSuccessful },
+    getFieldState,
+  } = useForm<IFormInput>();
+  const [urlServerError, setUrlServerError] = useState<string>();
   const onFormSubmit: SubmitHandler<IFormInput> = async (values) => {
     const res = await fetch("/api/addlink", {
       method: "POST",
@@ -27,7 +33,7 @@ const AddLinkForm: React.FC<IAddLinkFormProps> = ({ setLink }) => {
     });
     var parsedResponse: IAddLinkResponse = await res.json();
     if (!parsedResponse.validUrl) {
-      alert("invalid req");
+      setUrlServerError("You must provide a valid URL");
     } else {
       setLink(parsedResponse.link);
       reset();
@@ -37,11 +43,20 @@ const AddLinkForm: React.FC<IAddLinkFormProps> = ({ setLink }) => {
   return (
     <form onSubmit={handleSubmit(onFormSubmit)}>
       <input
-        {...register("url")}
+        aria-invalid={true}
+        {...register("url", { required: true })}
         type="text"
         placeholder="Enter your url here..."
         className="bg-gray-100 w-full px-2 h-12 placeholder-gray-600 mt-4 focus:outline-none focus:ring-2 focus:ring-indigo-700 rounded-lg"
       />
+
+      <p>
+        {getFieldState("url").invalid &&
+          !isSubmitSuccessful &&
+          "Please enter a URL"}
+      </p>
+      {urlServerError && <p>{urlServerError}</p>}
+
       <button className="mt-4 px-4 text-xl py-2 bg-indigo-700 hover:bg-indigo-600 text-white rounded-lg">
         Shorten
       </button>
